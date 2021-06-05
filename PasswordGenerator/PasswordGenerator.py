beaa9f84ceabd0e14d84fa8a740feee8fc909f49
bilal asghar <asgharbilal8@gmail.com>
2021-06-06 00:55:51 +0100

    check for the character that are going to be used in the password

--- PasswordGenerator/PasswordGenerator.py
+++ PasswordGenerator/PasswordGenerator.py
@@ -5,3 +5,19 @@ UPPERCASE_LETTERS = string.ascii_uppercase
 LOWERCASE_LETTERS = string.ascii_lowercase
 NUMBERS = string.digits
 SYMBOLS = string.punctuation
+
+upper_case = True
+lower_case = True
+numbers = True
+symbols = True
+
+characters_to_use = ""
+
+if upper_case:
+    characters_to_use += UPPERCASE_LETTERS
+if lower_case:
+    characters_to_use += LOWERCASE_LETTERS
+if numbers:
+    characters_to_use += NUMBERS
+if symbols:
+    characters_to_use += SYMBOLS
